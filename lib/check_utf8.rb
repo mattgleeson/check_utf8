14d63791269406a4db3f6e766675729b6d10b67e
Matt Gleeson <matt@gleeson.org>
2009-12-02 10:37:38 -0800

    infect everything

--- lib/check_utf8.rb
+++ lib/check_utf8.rb
@@ -1,22 +1,21 @@
 class CheckUtf8
-  def initialize(column_name)
-    @column_name = column_name
-  end
-
   def after_save(record)
-    val = record.send @column_name
-    if val && !val.is_utf8?
-      ActiveRecord::Base.logger.warn("class=#{record.class.name} id=#{record.id} column=#{@column_name} is not utf8!")
-    end
-  end
-end
-
-class ActiveRecord::Base
-  def self.check_utf8
-    columns.each do |c|
+    record.class.columns.each do |c|
       if [:string, :text].include?(c.type)
-        after_save CheckUtf8.new(c.name)
+        check_column(record, c.name)
       end
     end
   end
+
+  def check_column(record, column_name)
+    val = record[column_name]
+    if val && !val.to_s.is_utf8?
+      ActiveRecord::Base.logger.warn("CheckUtf8: class=#{record.class.name} id=#{record.id} column=#{column_name} is not utf8!")
+    else
+      ActiveRecord::Base.logger.debug("CheckUtf8: class=#{record.class.name} id=#{record.id} column=#{column_name} looks ok")
+    end
+  end
 end
+
+ActiveRecord::Base.after_save CheckUtf8.new
+
